5616be85988af8ea97c0a96d42f462b55837ae8a
Дмитрий Маркин <dima-markin-2000@yandex.ru>
2021-03-24 22:06:50 +0300

    Added app icon. Added dices functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -13,11 +14,44 @@ class DiceRollingPage extends StatefulWidget {
 }
 
 class _DiceRollingPageState extends State<DiceRollingPage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void setNewDicesNumbers() => setState(() {
+        leftDiceNumber = Random().nextInt(6) + 1;
+        rightDiceNumber = Random().nextInt(6) + 1;
+      });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.red.shade600,
       appBar: AppBar(
+        centerTitle: true,
         title: Text('Dice'),
+        backgroundColor: Colors.red.shade900,
+      ),
+      body: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: setNewDicesNumbers,
+                child: Image.asset(
+                  'images/dice$leftDiceNumber.png',
+                ),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: setNewDicesNumbers,
+                child: Image.asset(
+                  'images/dice$rightDiceNumber.png',
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
